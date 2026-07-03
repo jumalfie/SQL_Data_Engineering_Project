c57b71ec4989f6b3e04686211192d2eae326f470
Alfred Mutua <mutuaalfred0@gmail.com>
2026-07-03 06:40:52 -0600

    Corrected table values

--- 02_DW_Mart_Build/02_load_schema_dw.sql
+++ 02_DW_Mart_Build/02_load_schema_dw.sql
@@ -4,13 +4,10 @@ SELECT '===loading company_dim Table===' AS info;
 
 INSERT INTO company_dim (company_id, name)
 SELECT company_id, name
-FROM read_csv('https://storage.googleapis.com/sql_de/company_dim.csv',
+FROM read_csv('A:/Alfy/Downloads/company_dim.csv',
     AUTO_DETECT=true,
-     HEADER=true,
-      max_line_size=100_000_000,
-      null_padding=true,
-      ignore_errors=true
-     );
+    HEADER=true
+    );
 
 SELECT '===loading skills_dim Table===' AS info;
 
@@ -33,20 +30,44 @@ SELECT  job_id, company_id, job_title_short, job_title, job_location,
     job_via, job_schedule_type, job_work_from_home, search_location,
     job_posted_date, job_no_degree_mention, job_health_insurance, 
     job_country, salary_rate, salary_year_avg, salary_hour_avg
-FROM read_csv('https://storage.googleapis.com/sql_de/job_postings_fact.csv',
+FROM read_csv('A:/Alfy/Downloads/job_postings_fact.csv',
     AUTO_DETECT=true,
-     HEADER=true,
-     nullstr = ['N/A', '', 'NULL', 'missing'],
-     ignore_errors=true,
-     max_line_size = 100_000_000,
-     null_padding = true
+    HEADER=true
     );
 
 SELECT '===loading skills_job_dim Table===' AS info;
 
 INSERT INTO skills_job_dim (skill_id, job_id)
 SELECT skill_id, job_id
-FROM read_csv('https://storage.googleapis.com/sql_de/skills_job_dim.csv',
+FROM read_csv('A:/Alfy/Downloads/skills_job_dim.csv',
     AUTO_DETECT=true,
-     HEADER=true)
-WHERE job_id IN (SELECT job_id FROM job_postings_fact);
+     HEADER=true);
+
+
+SELECT 'Company Dim' AS table_name, COUNT(*) AS record_count FROM company_dim
+UNION ALL
+SELECT 'Skill Dim', COUNT(*) FROM skills_dim
+UNION ALL 
+SELECT 'Job Postings Fact', COUNT(*) FROM job_postings_fact
+UNION ALL 
+SELECT 'Skill Job Dim', COUNT(*) FROM skills_job_dim;
+
+SELECT '=== Company Dimension Sample===' AS info;
+SELECT * 
+FROM company_dim
+LIMIT 10;
+
+SELECT '=== Skills Dimension Sample===' AS info;
+SELECT * 
+FROM skills_dim
+LIMIT 10;
+
+SELECT '=== Job Postings Fact Sample===' AS info;
+SELECT * 
+FROM job_postings_fact
+LIMIT 10;
+
+SELECT '=== Skills Job Bridge Sample===' AS info;
+SELECT * 
+FROM skills_job_dim
+LIMIT 10;
